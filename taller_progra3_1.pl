4366977ff5082881e1dabf3fc8af54e2f8554d05
tamayo2 <juan.tamayo3@utp.edu.co>
2026-02-23 23:21:50 -0500

    second commit

--- taller_progra3_1.pl
+++ taller_progra3_1.pl
@@ -76,3 +76,16 @@ primos(X, Y) :-
     madre(Z, X),
     madre(A, Y),
     hermana(Z, A).
+
+
+% Hechos
+es_estadounidense(west).
+es_enemigo_de(corea_sur, usa).
+le_vendio_armas_a(west, corea_sur).
+
+% Regla
+es_criminal(X) :-
+es_estadounidense(X),
+es_enemigo_de(Y, usa),
+le_vendio_armas_a(X, Y).
+
